195128132a8b11caa4e17606a4e0ff1228f590f7
Vishal-Agarwal77 <108511250+Vishal-Agarwal77@users.noreply.github.com>
2023-11-24 03:29:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //your JS code here. If required.
+const box=document.getElementsByClassName("square");
+for(let item of box){
+	// console.log(item);
+	item.addEventListener("mouseover",()=>{
+	 	for(let it of box){
+			if(it.id!=item.id){
+				it.style.backgroundColor="#6F4E37";
+			}
+		}
+	})
+	item.addEventListener("mouseout",()=>{
+	 	for(let it of box){
+			it.style.backgroundColor="#E6E6FA";
+		}
+	})
+};
